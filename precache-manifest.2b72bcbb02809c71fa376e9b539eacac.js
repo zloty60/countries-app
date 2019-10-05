8410bf9bf06a821752e38bf311bd71e6a0ddf21c
Przemek <przemek.bzowka@gmail.com>
2019-10-05 22:57:13 +0200

    Updates

---
+++ precache-manifest.2b72bcbb02809c71fa376e9b539eacac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbddf0b10888ab827dd3f59e91d56d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d462c8c50f456ec68a1c",
+    "url": "/static/css/main.3dbb7c61.chunk.css"
+  },
+  {
+    "revision": "28d2eb280a0e1bce8777",
+    "url": "/static/js/2.3fb57e5f.chunk.js"
+  },
+  {
+    "revision": "d462c8c50f456ec68a1c",
+    "url": "/static/js/main.0455f937.chunk.js"
+  },
+  {
+    "revision": "b8e6ccbaa1bd44afc798",
+    "url": "/static/js/runtime~main.67a5e9ca.js"
+  },
+  {
+    "revision": "1362bc7161f38bad888498f5e04edaf0",
+    "url": "/static/media/closeIcon.1362bc71.svg"
+  },
+  {
+    "revision": "36854c33a220172c91596d757c578319",
+    "url": "/static/media/globe.36854c33.svg"
+  },
+  {
+    "revision": "c3a732cdaf5b404041e05cefe032ae44",
+    "url": "/static/media/leftArrowIcon.c3a732cd.svg"
+  },
+  {
+    "revision": "e138a4175e58b9b334705d3aceadcddb",
+    "url": "/static/media/menuIcon.e138a417.svg"
+  },
+  {
+    "revision": "a457c526ff40e2b143db2177af0c4bd9",
+    "url": "/static/media/sadIcon.a457c526.svg"
+  },
+  {
+    "revision": "34d366898f28c856c24fee651faaf38c",
+    "url": "/static/media/searchIcon.34d36689.svg"
+  },
+  {
+    "revision": "2a3eefaa9709a1baa1757cf36a812135",
+    "url": "/static/media/star.2a3eefaa.svg"
+  },
+  {
+    "revision": "a1d74360db25b3951250d3c593a823f8",
+    "url": "/static/media/starOutline.a1d74360.svg"
+  }
+]);
