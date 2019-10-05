537f33ecba276299fa4bc0b3508ff6bf2f4593f7
Przemek <przemek.bzowka@gmail.com>
2019-10-05 23:06:58 +0200

    Updates

---
+++ precache-manifest.e8194fb9725e82a9136fdefaea70e238.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd2c16332d3795677a0027edc9c4a0d",
+    "url": "/countries-app/index.html"
+  },
+  {
+    "revision": "d462c8c50f456ec68a1c",
+    "url": "/countries-app/static/css/main.3dbb7c61.chunk.css"
+  },
+  {
+    "revision": "28d2eb280a0e1bce8777",
+    "url": "/countries-app/static/js/2.3fb57e5f.chunk.js"
+  },
+  {
+    "revision": "d462c8c50f456ec68a1c",
+    "url": "/countries-app/static/js/main.0455f937.chunk.js"
+  },
+  {
+    "revision": "c2283c7b392bad94fa6b",
+    "url": "/countries-app/static/js/runtime~main.9fb72913.js"
+  },
+  {
+    "revision": "1362bc7161f38bad888498f5e04edaf0",
+    "url": "/countries-app/static/media/closeIcon.1362bc71.svg"
+  },
+  {
+    "revision": "36854c33a220172c91596d757c578319",
+    "url": "/countries-app/static/media/globe.36854c33.svg"
+  },
+  {
+    "revision": "c3a732cdaf5b404041e05cefe032ae44",
+    "url": "/countries-app/static/media/leftArrowIcon.c3a732cd.svg"
+  },
+  {
+    "revision": "e138a4175e58b9b334705d3aceadcddb",
+    "url": "/countries-app/static/media/menuIcon.e138a417.svg"
+  },
+  {
+    "revision": "a457c526ff40e2b143db2177af0c4bd9",
+    "url": "/countries-app/static/media/sadIcon.a457c526.svg"
+  },
+  {
+    "revision": "34d366898f28c856c24fee651faaf38c",
+    "url": "/countries-app/static/media/searchIcon.34d36689.svg"
+  },
+  {
+    "revision": "2a3eefaa9709a1baa1757cf36a812135",
+    "url": "/countries-app/static/media/star.2a3eefaa.svg"
+  },
+  {
+    "revision": "a1d74360db25b3951250d3c593a823f8",
+    "url": "/countries-app/static/media/starOutline.a1d74360.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b72bcbb02809c71fa376e9b539eacac.js"
+  "/countries-app/precache-manifest.e8194fb9725e82a9136fdefaea70e238.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/countries-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
